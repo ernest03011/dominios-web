834b7efba75e694700d00ce0df135043a12e769b
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-04-15 18:48:59 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const player1 = document.getElementById("first-list");
+const player2 = document.getElementById("second-list");
+
+const firstPlayerInput = document.getElementById("first-player-score");
+const secondPlayerInput = document.getElementById("second-player-score");
+
+// const addScoreButtons = document.querySelectorAll("button");
+const addScoreButtons = document.querySelectorAll(".card__players-btn");
+const newGameBtn = document.getElementById("new-game-btn");
+
+const cardErrorMessage = document.getElementById("card-error-message");
+
+newGameBtn.addEventListener("click", () => startNewgame());
+
+// Check if a given value is positive and between 0 and 200
+
+const validNumberInDominio = (input) => {
+  const re = /^(?:[1-9]|[1-9][0-9]|1[0-9]{2}|200)$/;
+  return re.test(input);
+};
+
+// Clear Score board
+
+const clearScore = () => {
+  firstPlayerInput.value = "";
+  secondPlayerInput.value = "";
+};
+
+// Displaying score to the list
+
+const displayScore = (button) => {
+  let item = document.createElement("li");
+
+  const playerInput = button.getAttribute("aria-controls");
+
+  if (playerInput === "first-player-score") {
+    if (validNumberInDominio(firstPlayerInput.value)) {
+      let itemText = document.createTextNode(firstPlayerInput.value);
+
+      item.appendChild(itemText);
+      player1.appendChild(item);
+    } else {
+      displayError();
+    }
+  } else if (playerInput === "second-player-score") {
+    if (validNumberInDominio(secondPlayerInput.value)) {
+      let itemText = document.createTextNode(secondPlayerInput.value);
+
+      item.appendChild(itemText);
+      player2.appendChild(item);
+    } else {
+      displayError();
+    }
+  }
+
+  clearScore();
+};
+
+addScoreButtons.forEach((button) => {
+  button.addEventListener("click", () => displayScore(button));
+});
+
+// Resetting the values to 0 in order to start a new Game
+
+const startNewgame = () => {
+  let item = `<li>0</li>`;
+
+  player1.innerHTML = item;
+  player2.innerHTML = item;
+
+  clearScore();
+};
+
+//END -- Resetting the values to 0 in order to start a new Game
+
+// Displaying error message
+
+const displayError = () => {
+  cardErrorMessage.classList.remove("hidden-visible");
+  setTimeout(() => {
+    cardErrorMessage.classList.add("hidden-visible");
+  }, 3000);
+};
+
+// END -- Displaying error message
---
+++ notes.md
@@ -0,0 +1,31 @@
+# Things to note
+
+## Things that I might add/delete/remove later one
+
+### Functionalities
+
+1. Add a section to display the total. A sum of the score
+2. Limit the score 200 and display a message letting know X user has won.
+3. Showing specific alert letting know that input value can not be empty or that it does not accept text
+4. Based on what was added, I could display an alert that shows "XY is not a valid input in Dominio"
+
+### Design stuff that I might edit/add/remove
+
+1. New game button looks too big on Desktop sizes.
+2. Making the paragraph in the footer more legible. It is difficult to read when the background is black at times depending on the sizes.
+
+---
+
+## Feedback
+
+- Empty for now.
+
+## Possible ToDos
+
+- Set a reponsive width to main card, it could be using percentage, like 80, and then center it. As well as adding a max width
+- Refactor both CSS and js code
+- Add a total display
+- Enabling to delete that item, only if, the sum is more than 0
+- Adding another page where the user can add the amount of players and their names
+- Having a button to reset all values, and a button to start a new game.
+- Readjusting the sizes for the heading and card after 1200px width. The button gets too big as well.
