50d29d1a09db0e4c386162d3ee293f3c8c223fba
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-06-02 07:42:35 -0400

    convert isValidScore method to an static one

--- js/player.js
+++ js/player.js
@@ -33,7 +33,7 @@ export class Player {
     return total;
   }
 
-  isValidScore(score) {
+  static isValidScore(score) {
     const re = /^(?:[1-9]|[1-9][0-9]|1[0-9]{2}|200)$/;
     return re.test(score);
   }
